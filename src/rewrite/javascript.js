5a2cb4383751bee84c398b4a3d14af72849759e0
Nebelung <nebelung@mailfence.com>
2024-07-22 09:08:11 +0000

    JS backend rewrites

--- src/rewrite/javascript.js
+++ src/rewrite/javascript.js
@@ -1,5 +1,36 @@
+import { parseModule } from "meriyah";
+import { generate } from "astring";
+
 function javascript(code, origin) {
-    return code;
+    try {
+        const ast = parseModule(code, {
+            module: true,
+            webcompat: true,
+            globalReturn: true
+        })
+
+        const globals = ["window", "self", "globalThis", "this", "parent", "top", "location", "document"];
+
+        function traverse(node) {
+            if (!node) return;
+
+            if (node.type == "Identifier" && globals.includes(node.name)) {
+                node.name = `__eclipse$scope(${node.name})`;
+            }
+
+            for (let key in node) {
+                if (node[key] && typeof node[key] == "object") {
+                    traverse(node[key]);
+                }
+            }
+        }
+
+        traverse(ast);
+
+        return generate(ast);
+    } catch {
+        return "";
+    }
 }
 
 export { javascript };
