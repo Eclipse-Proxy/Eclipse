34c5114a8f0086943a25d6ff593ccf25b16eb3aa
Nebelung <nebelung@mailfence.com>
2024-07-22 10:09:25 +0000

    Frontend

--- public/index.js
+++ public/index.js
@@ -6,6 +6,13 @@
     const connection = new window.BareMux.BareMuxConnection("/baremux/worker.js");
     await connection.setTransport("/libcurl/index.mjs", [{ wisp: (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/" }]);
 
-    const example = document.querySelector("#example");
-    example.href = self.__eclipse$config.prefix + self.__eclipse$config.codec.encode("https://example.com");
+    const search = document.querySelector("#search");
+
+    search.addEventListener("keyup", function(e) {
+        if (e.target.value && e.key == "Enter") {
+          e.preventDefault();
+          window.location.href = self.__eclipse$config.prefix + self.__eclipse$config.codec.encode(e.target.value);
+          e.target.value = "";
+        }
+      });
 })();
